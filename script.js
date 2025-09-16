e6244588274d9f3c1993f30ce32add40611e320d
Aymane <aymanesalmoune28@gmail.com>
2025-09-16 16:01:21 +0100

    adding javascript script

---
+++ script.js
@@ -0,0 +1,10 @@
+let number1 = 5;
+let number2 = 10;
+let sum = number1 + number2;
+console.log("The sum is: " + sum);
+
+function  addition(a, b) {
+    return a + b;
+}
+let sum2 = addition(3, 7);
+console.log("The sum is: " + sum2);
